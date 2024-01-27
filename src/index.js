bc2e4792991e5b41bafc5f09a9e343fab848e054
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2024-01-28 08:40:24 +0900

    added API info

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let apiKey = "8155of93a73b3d00et993d1e4ab007f6";
+let context = "you are an AI assistant who loves pun and jokes about animals";
+let prompt = "please tell me a joke about animals. please keep the joke short in length. thank you";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
