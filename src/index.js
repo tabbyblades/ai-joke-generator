8623945a9b07dd53dbb6907aa39c73c81fb2dab6
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2024-01-28 09:01:30 +0900

    created getJoke function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-function showAnswer (response){
+function showAnswer(response){
     alert(response.data.answer);
 }
 
-
+function getJoke () {
 let apiKey = "8155of93a73b3d00et993d1e4ab007f6";
 let context = "you are an AI assistant who loves pun and jokes about animals";
 let prompt = "please tell me a joke about animals. please keep the joke short in length. thank you";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 axios.get(apiUrl).then(showAnswer);
+}
+
+let jokeButton = document.querySelector("button");
+jokeButton.addEventListener("click",getJoke);
