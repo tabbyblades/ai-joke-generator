0908891a8099d79789c1ec3c2d67a1b40ffb3140
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2024-01-30 08:10:25 +0900

    updated the prompt

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ function showAnswer(response){
     new Typewriter('#joke', {
         strings: [(response.data.answer)],
         autoStart: true,
+        cursor: null,
       });
 }
 
 function getJoke () {
 let apiKey = "8155of93a73b3d00et993d1e4ab007f6";
 let context = "you are an AI assistant who loves pun and jokes about animals";
-let prompt = "please tell me a joke about animals. please keep the joke short in length. thank you";
+let prompt = "please tell me a joke about animals. please keep the joke short in length and only tell me the joke and the answer";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 axios.get(apiUrl).then(showAnswer);
